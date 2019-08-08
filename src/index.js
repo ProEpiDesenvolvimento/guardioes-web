62f120ea7bd8b1d86b9febe08db5edc56a2cf5dc
root <root@painel.gds.proepi.org.br>
2019-08-08 14:38:38 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import '@fortawesome/fontawesome-free/css/all.min.css';
 // import 'bootstrap-css-only/css/bootstrap.min.css';
 // import 'mdbreact/dist/css/mdb.css';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
