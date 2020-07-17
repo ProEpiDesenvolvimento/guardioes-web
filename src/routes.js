d9a234bb002ce1f4af5b97132784552566a52738
Tiago <tsrrodrigues@outlook.com>
2020-07-17 16:38:52 -0300

    Adding right routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from './pages/Home/';
 import PreRegister from 'pages/PreRegister';
+import Thanks from 'pages/Thanks'
 
 
 export default function Routes() {
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/preregister' exact component={PreRegister} />
+        <Route path='/thanks' exact component={Thanks} />
       </Switch>
     </BrowserRouter>
   );
