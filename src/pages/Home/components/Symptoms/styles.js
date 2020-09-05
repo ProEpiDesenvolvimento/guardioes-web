60e059f6a2fa1c302d0bd1372648719984bfb243
emanuks <emanuels313@gmail.com>
2020-09-05 18:54:20 -0300

    Adding edit modal to symptoms

--- src/pages/Home/components/Symptoms/styles.js
+++ src/pages/Home/components/Symptoms/styles.js
@@ -98,3 +98,42 @@ export const SubmitButton = styled.button`
     opacity: 0.7;
   }
 `;
+
+export const EditInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-weight: 700;
+  font-size: 24;
+  color: #348EAC;
+  margin-bottom: 1rem;
+`;
+
+export const TextArea = styled.textarea`
+  resize: none;
+  border: solid 1px #CCCCCC;
+  border-radius: 5px;
+  padding: 5px 5px 10px 10px;
+
+  &:focus {
+    border: solid 2px #007bff;
+    padding: 4px 4px 9px 9px;
+`;
+
+export const EditButton = styled.button`
+  width: 100%;
+  background-color: #5DD39E;
+  color: white;
+  border: 0;
+  border-radius: 5px;
+  height: 2.2rem;
+  cursor: pointer;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  @media (min-width: 500px) {
+    width: 14rem;
+  }
+`;
