edd1f5d5bf1a0405a4d52684bde21340c3ac9b9e
Tiago Samuel Rodrigues <31369018+tsrrodrigues@users.noreply.github.com>
2020-10-08 01:02:39 -0300

    Update index.js

--- src/sharedComponents/Header/index.js
+++ src/sharedComponents/Header/index.js
@@ -3,7 +3,8 @@ import {
   Container,
   Logo,
   HeaderNav,
-  NavTo
+  NavTo,
+  Logout
 } from './styles';
 import logo from 'utils/assets/logo.png';
 import { sessionService } from 'redux-react-session';
