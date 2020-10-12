4520919951b546d2e25fef898692d8b7d3128757
Gabriel Martins <gsmartins96@hotmail.com>
2020-10-12 18:15:59 -0300

    start manager page

---
+++ src/pages/Home/components/Manager/services/createManager.js
@@ -0,0 +1,20 @@
+import api from 'services/api';
+
+const createManagers = async (data, token) =>
+  api.post(`/manager/signup`, data,
+    {
+      headers: {
+        "Authorization": token,
+      }
+    }
+  )
+    .then(async (res) => {
+      const response = { data: res.data };
+      return response
+    })
+    .catch((e) => {
+      alert('Algo deu errado, tente novamente!');
+      console.log(e);
+      return { data: {}, errors: e }
+    });
+export default createManagers;
