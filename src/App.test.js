4a52e5c132ec33be20fb2ecdd3d59f23cda636e9
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2020-10-15 23:59:13 -0300

    Commenting test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,11 @@
-it('test', () => {
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+it('renders without crashing', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
   expect(3).toBe(3)
 });
