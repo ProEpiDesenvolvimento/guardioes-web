f5574f65fcf8b6fbe1439b22d47b13541abddc41
Italo Alves <italooko@gmail.com>
2021-01-15 10:20:18 -0300

    Fix users NavBar options

--- src/pages/Home/components/NavBar/index.js
+++ src/pages/Home/components/NavBar/index.js
@@ -76,15 +76,15 @@ const NavBar = ({
 
     if (user.type === "admin") {
       if (user.is_god === true) {
-        categories = categories.concat(allCategories.slice(1, -2));
+        categories = categories.concat(allCategories.slice(1));
       }
       else {
-        categories = categories.concat(allCategories.slice(3));
+        categories = categories.concat(allCategories.slice(3, -2));
       }
     } else if (user.type === "manager") {
-      categories = categories.concat(allCategories.slice(5));
+      categories = categories.concat(allCategories.slice(5, -2));
     } else if (user.type === "group_manager") {
-      categories = categories.concat(allCategories.slice(8));
+      categories = categories.concat(allCategories.slice(8, -2));
     }
 
     setCategories(categories)
