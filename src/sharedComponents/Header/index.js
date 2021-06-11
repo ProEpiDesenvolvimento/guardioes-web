a46f12abe3b9dc5adc647bd47c9d31c0cfdc2c08
Ítalo Alves <italooko@gmail.com>
2021-06-10 21:48:50 -0300

    Add GroupManagerTeam for login and forget password

--- src/sharedComponents/Header/index.js
+++ src/sharedComponents/Header/index.js
@@ -27,6 +27,7 @@ const Header = ({
     "manager": "Gerente",
     "city_manager": "Município",
     "group_manager": "Instituição",
+    "group_manager_team": "Grupo de Instituição",
   }
 
   useEffect(() => {
