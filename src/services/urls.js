cb7717caa1a48b3ecf9e2df148e234de38ae8100
Joao Pedro <jps12carvalho@gmail.com>
2021-06-13 15:36:12 -0300

    Fixing when didn't have syndromes indexed

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,6 +1,7 @@
 //Set patch variables
 
-const apiURL = 'http://localhost:3001'
+// const apiURL = 'http://localhost:3001'
+const apiURL = 'http://homolog.gds.proepi.org.br'
 const godataURL = 'http://localhost:3001'
 
 export {apiURL, godataURL}
