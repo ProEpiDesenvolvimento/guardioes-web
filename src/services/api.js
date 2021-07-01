b9464829dd7af2b5740da6a856e46455afbb06f8
Ítalo Alves <italooko@gmail.com>
2021-07-01 14:19:07 -0300

    Fix axios default headers

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import { apiURL } from 'services/urls'
 
 axios.defaults.baseURL = apiURL;
-axios.defaults.responseType = 'json';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common.Accept = 'application/json';
+axios.defaults.headers.common.Accept = 'application/vnd.api+json';
 
 export default axios;
