b3421ce88b47845cdef8a46a1c058296a1ade1c9
gabrieldouurado <41026043+gabrieldouurado@users.noreply.github.com>
2021-07-02 14:20:49 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { apiURL } from 'services/urls'
 
 axios.defaults.baseURL = apiURL;
+axios.defaults.responseType = 'json';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common.Accept = 'application/vnd.api+json';
+axios.defaults.headers.common.Accept = 'application/json';
 
 export default axios;
