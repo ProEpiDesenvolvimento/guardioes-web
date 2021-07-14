6d2d55f92b1e006b03cb88c741c52cf449f77571
Ítalo Alves <italooko@gmail.com>
2021-07-14 14:39:37 -0300

    Add GroupManagerTeam to Dashboard page

--- src/pages/Home/components/Dashboard/index.js
+++ src/pages/Home/components/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {
@@ -6,32 +7,22 @@ import {
   setToken
 } from 'actions/';
 import { sessionService } from 'redux-react-session';
-
-import queryString from 'query-string';
-
-import getGraphs from './services/getGraphs'
-
 import { isEmpty } from "lodash";
+
+import getGraphs from './services/getGraphs';
 import './styles.css';
 
 const Dashboard = ({
   token,
   user
-}, props) => {
+}) => {
   const [urls, setUrls] = useState({})
   const [hashes, setHashes] = useState(queryString.parse(window.location.hash))
 
-  if (isEmpty(hashes)){
-    setHashes({surveys: null})
-  }
-
   const _getUrls = async (type) => {
-    if(!type){
+    if (!type) {
       type = 'surveys'
-    } else {
-      type = Object.keys(hashes)[0]
     }
-
     const response = await getGraphs(token, type)
     setUrls(response.urls)
   }
@@ -49,41 +40,70 @@ const Dashboard = ({
     } else {
       setHashes(hashes)
     }
-
-    console.log(urls)
-  }, [hashes.surveys, hashes.users, hashes.biosecurity, hashes.vigilance, user, token]);
+  }, [user, token, hashes.surveys, hashes.users, hashes.biosecurity, hashes.vigilance]);
   
-  const isHashes = (string) => {
-    if (typeof hashes[string] !== "undefined") return true
+  const isHash = (string) => {
+    if (typeof hashes[string] !== "undefined") {
+      return true
+    }
     return false
   }
 
-  const isGroupManager = () => user.type === 'group_manager';
-  
+  const isGroupManagerORTeam = () => {
+    return user.type === 'group_manager' || user.type === 'group_manager_team'
+  }
+
   const isVigilance = () => {
-    if (user.vigilance_email != null) {
-      if (user.vigilance_email.length > 0) return true
-    } else return false
+    if (user.vigilance_email) {
+      if (user.vigilance_email.length > 0) {
+        return true
+      }
+    }
+    return false
   }
 
   // This allows a begin date for iframes
-  let date = props.date || '2020-06-10T07:01:16.923Z' // App launch date
+  // let date = props.date || '2020-06-10T07:01:16.923Z' // App launch date
 
   return (
     <div style={{ height: '100%' }}>
       <div className="dash visualizations">
         <ul className="nav nav-tabs justify-content-center" style={{ marginTop: 20 }}>
           <li className="nav-item">
-            <a onClick={() => setHashes({ surveys: null })} className={`nav-link ${isHashes('surveys') ? 'active' : ''}`} href="#surveys">Reportes</a>
+            <a 
+              onClick={() => setHashes({ surveys: null })} 
+              className={`nav-link ${isHash('surveys') ? 'active' : ''}`} 
+              href="#surveys"
+            >
+              Reportes
+            </a>
           </li>
           <li className="nav-item">
-            <a onClick= {() => setHashes({ users: null })} className={`nav-link ${isHashes('users') ? 'active' : ''}`} href="#users">Usuários</a>
+            <a
+              onClick= {() => setHashes({ users: null })}
+              className={`nav-link ${isHash('users') ? 'active' : ''}`}
+              href="#users"
+            >
+              Usuários
+            </a>
           </li>
           <li className="nav-item">
-            <a onClick={() => setHashes({ biosecurity: null })} className={`nav-link ${ !isGroupManager() ? 'd-none': ''} ${isHashes('biosecurity') ? 'active' : ''}`} href="#biosecurity">Biosegurança</a>
+            <a
+              onClick={() => setHashes({ biosecurity: null })}
+              className={`nav-link ${!isGroupManagerORTeam() ? 'd-none': ''} ${isHash('biosecurity') ? 'active' : ''}`}
+              href="#biosecurity"
+            >
+              Biossegurança
+            </a>
           </li>
           <li className="nav-item">
-          <a onClick={() => setHashes({ vigilance: null })} className={`nav-link ${ !isGroupManager() || !isVigilance() ? 'd-none': ''} ${isHashes('vigilance') ? 'active' : ''}`} href="#vigilance">Vigilância Ativa</a>
+            <a
+              onClick={() => setHashes({ vigilance: null })}
+              className={`nav-link ${!isGroupManagerORTeam() || !isVigilance() ? 'd-none': ''} ${isHash('vigilance') ? 'active' : ''}`}
+              href="#vigilance"
+            >
+              Vigilância Ativa
+            </a>
           </li>
         </ul>
       </div>
