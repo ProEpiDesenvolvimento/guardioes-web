e1031154fe0350a778232ccc04314ef86f829e40
Ítalo Alves <italooko@gmail.com>
2021-09-04 16:41:37 -0300

    Change TableDragAndDrop

--- src/pages/Home/components/ContentBox/TableDragAndDrop/index.js
+++ src/pages/Home/components/ContentBox/TableDragAndDrop/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 import { 
   ContentBoxTableHeader,
   ContentBoxTableIcon,
- } from '../Table/styles';
+} from '../Table/styles';
+import { Tr } from './styles';
 import { Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import editIcon from 'pages/Home/components/assets/edit-solid.svg';
@@ -13,77 +16,110 @@ import cancelIcon from 'pages/Home/components/assets/cancel.svg';
 const TableDragAndDrop = ({
   contents,
   fields,
+  setContentsOrder,
   setContentShow,
   setEditingContent,
   _deleteApp,
   token
 }) => {
-  const [confirmDelete, setConfirmDelete] = useState(null)
+  const [confirmDelete, setConfirmDelete] = useState(null);
+
+  const handleContentOnDragEnd = (e) => {
+    if (!e.destination) return;
+
+    let newContents = contents.slice();
+    const [reorderedOption] = newContents.splice(e.source.index, 1);
+    newContents.splice(e.destination.index, 0, reorderedOption);
+
+    newContents = newContents.map((option, index) => {
+      return { ...option, order: index+1 }
+    });
+
+    setContentsOrder(newContents);
+  }
 
   const handleDelete = (id, token) => {
     if (confirmDelete === id) {
-      _deleteApp(id, token)
-      setConfirmDelete(null)
+      _deleteApp(id, token);
+      setConfirmDelete(null);
     } else {
       setConfirmDelete(id);
     }
   }
 
   return (
-    <Table responsive>
-      <thead>
-        <tr>
-          {fields.map(field => (
-            <ContentBoxTableHeader style={{maxWidth: "500px"}} key={field.key}>{field.value}</ContentBoxTableHeader>
-          ))}
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {contents.map(content => (
-          <tr key={content.id}>
+    <DragDropContext onDragEnd={handleContentOnDragEnd}>
+      <Table responsive>
+        <thead>
+          <tr>
             {fields.map(field => (
-              <td style={{maxWidth: "500px"}} key={field.key}>{content[field.key]}</td>
+              <ContentBoxTableHeader style={{maxWidth: "500px"}} key={field.key}>{field.value}</ContentBoxTableHeader>
             ))}
-            <td>
-              <Link to="/panel">
-                <button className="btn btn-info" onClick={() => { setContentShow(content) }}>
-                  Visualizar
-                </button>
-              </Link>
-            </td>
-            <td>
-              <Link to="/panel">
-                <ContentBoxTableIcon
-                  src={editIcon}
-                  alt="Editar"
-                  onClick={() => { setEditingContent(content) }}
-                />
-              </Link>
-            </td>
-            <td>
-              <Link to="/panel">
-                <ContentBoxTableIcon
-                  cursor={"true"}
-                  src={confirmDelete === content.id ? confirmIcon : deleteIcon}
-                  alt="Deletar"
-                  onClick={() => { handleDelete(content.id, token) }}
-                />
-                <ContentBoxTableIcon
-                  cursor={confirmDelete === content.id ? "true" : "false"}
-                  style={{width: "20px", marginLeft: "10px", opacity: confirmDelete === content.id ? 1 : 0}}
-                  src={cancelIcon}
-                  alt="Deletar"
-                  onClick={() => { setConfirmDelete(null) }}
-                />
-              </Link>
-            </td>
+            <th></th>
+            <th></th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+
+        <Droppable droppableId="edit-content">
+          {(provided) => (
+            <tbody className="droppable" {...provided.droppableProps} ref={provided.innerRef}>
+              {contents.map((content, index) => (
+                <Draggable key={content.id} draggableId={content.id.toString()} index={index}>
+                  {(provided) => (
+                    <Tr
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                      title="Arraste para organizar"
+                    >
+                      {fields.map(field => (
+                        <td style={{maxWidth: "500px"}} key={field.key}>{content[field.key]}</td>
+                      ))}
+                      <td>
+                        <Link to="/panel">
+                          <button className="btn btn-info" onClick={() => { setContentShow(content) }}>
+                            Visualizar
+                          </button>
+                        </Link>
+                      </td>
+                      <td>
+                        <Link to="/panel">
+                          <ContentBoxTableIcon
+                            cursor={"true"}
+                            src={editIcon}
+                            alt="Editar"
+                            onClick={() => { setEditingContent(content) }}
+                          />
+                        </Link>
+                      </td>
+                      <td>
+                        <Link to="/panel">
+                          <ContentBoxTableIcon
+                            cursor={"true"}
+                            src={confirmDelete === content.id ? confirmIcon : deleteIcon}
+                            alt="Deletar"
+                            onClick={() => { handleDelete(content.id, token) }}
+                          />
+                          <ContentBoxTableIcon
+                            cursor={confirmDelete === content.id ? "true" : ""}
+                            style={{width: "20px", marginLeft: "10px", opacity: confirmDelete === content.id ? 1 : 0}}
+                            src={cancelIcon}
+                            alt="Deletar"
+                            onClick={() => { setConfirmDelete(null) }}
+                          />
+                        </Link>
+                      </td>
+                    </Tr>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </tbody>
+          )}
+        </Droppable>
+      </Table>
+    </DragDropContext>
   );
 }
 
---
+++ src/pages/Home/components/ContentBox/TableDragAndDrop/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Tr = styled.tr`
+  background-color: #E5E5E5;
+`;
