981792e96e298e90e0904084119987182342dc4e
Ítalo Alves <italooko@gmail.com>
2021-11-24 11:21:12 -0300

    Add ID label for User on Users page when Admin

--- src/pages/Home/components/Users/index.js
+++ src/pages/Home/components/Users/index.js
@@ -404,6 +404,18 @@ const Users = ({
         </Modal.Header>
 
         <Modal.Body>
+          {user.type === 'admin' ?
+            <EditInput>
+              <label>ID</label>
+              <input
+                className="text-dark"
+                type="text"
+                value={userShow.id}
+                disabled
+              />
+            </EditInput>
+          : null}
+
           <EditInput>
             <label>Birthdate</label>
             <input
@@ -413,7 +425,6 @@ const Users = ({
               disabled
             />
           </EditInput>
-
 
           <EditInput>
             <label>Email</label>
